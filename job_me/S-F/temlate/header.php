176dc781924bad464322fb4987274fef2ddf0b70
easyOlly <123547162+easyOlly@users.noreply.github.com>
2023-02-23 16:14:23 +0000

    Add files via upload

---
+++ job_me/S-F/temlate/header.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Mini project</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="">
+    
+  
+   
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/style.css">
+  </head>
---
+++ job_me/S-F/temlate/script.php
@@ -0,0 +1,4 @@
+<script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/main.js"></script>
