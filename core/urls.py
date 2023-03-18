aaf99221d1b37e3c746fb222685ffd41b71cc727
Abiola Olatunbosun <olaide_abiola87@yahoo.com>
2023-03-18 20:51:21 +0000

    updated core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
-
 from . import views
 from .views import *
 from .forms import JobSeekerLoginForm, OrganizationLoginForm
@@ -15,5 +14,5 @@ urlpatterns = [
     path('organization-signup/', views.organization_signup, name='organization-signup'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=JobSeekerLoginForm), name='login'),
     path('organization-login/', auth_views.LoginView.as_view(template_name='core/organization-login.html', authentication_form=OrganizationLoginForm), name='organization-login'),
-    path('logout', LogoutView.as_view(), name='logout')
+    path('logout', LogoutView.as_view(), name='logout'),
 ]
