7e16f82f38ee6c7f559d613a27bfafd0ef373e47
Abiola Olatunbosun <olaide_abiola87@yahoo.com>
2023-03-18 20:24:05 +0000

    added logout core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
+from .views import *
 from .forms import JobSeekerLoginForm, OrganizationLoginForm
 
 app_name = 'core'
@@ -14,4 +15,5 @@ urlpatterns = [
     path('organization-signup/', views.organization_signup, name='organization-signup'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=JobSeekerLoginForm), name='login'),
     path('organization-login/', auth_views.LoginView.as_view(template_name='core/organization-login.html', authentication_form=OrganizationLoginForm), name='organization-login'),
+    path('logout', LogoutView.as_view(), name='logout')
 ]
