78440c9d17c97738d3aa492442f722ff42d4d791
Abiola Olatunbosun <olaide_abiola87@yahoo.com>
2023-03-19 20:58:18 +0000

    updated core/models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    is_applicant = models.BooleanField(default=False)
+    is_organization = models.BooleanField(default=False)
